43303ca9529701162d72c641c30859dbabb17d15
ayyush1738 <singhrathoreayush824@gmail.com>
2025-02-16 22:18:30 +0530

    latest

--- Frontend/src/Components/chatMessage.jsx
+++ Frontend/src/Components/chatMessage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
-import chatImage from '../../public/chat.png';
+import chatImage from '../.././public/chat.png';
 import ReactMarkdown from "react-markdown";
 
 function Chat() {
